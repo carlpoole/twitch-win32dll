d09718603a5be760b5975d244d2fb59c56c3fa02
Carl Poole <carl@ionic.io>
2024-02-19 09:55:28 -0600

    better time/date format on logs

--- utils/logger.js
+++ utils/logger.js
@@ -1,13 +1,20 @@
 function logError(error) {
-    console.error(`[${new Date().toISOString()}]: Error: ${error}`);
+    console.error(`[${date()}]: Error: ${error}`);
 }
 
 function log(message) {
-    console.log(`[${new Date().toISOString()}]: ${message}`);
+    console.log(`[${date()}]: ${message}`);
 }
 
 function logLine() {
     console.log('----------------------------------------');
+}
+
+function date() {
+    return new Intl.DateTimeFormat('en-US', {
+        dateStyle: 'short',
+        timeStyle: 'long'
+      }).format(new Date())
 }
 
 module.exports = {
